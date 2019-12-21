4e194ede7b3b82100c5b427d0f769ae4e8a907a1
True Ghiassi <true@ghiassitrio.co.uk>
2019-12-21 21:58:45 +0000

    The labeller is now done.

--- labeller.py
+++ labeller.py
@@ -1,27 +1,45 @@
 import bottle
+import json
+import os
 
 
-@bottle.get('/static/<filename>')
-def getmain(filename):
-    return bottle.static_file(filename, root='html')
+@bottle.get('/')
+def getmain():
+    return bottle.static_file('index.html', root='.')
 
 
-imageFileNames = os.listdir('photos')
-counter = 0
-
-
-@bottle.get('/image')
-def getimage():
-    return bottle.static_file(imageFileNames[counter], root='photos')
+def remove_done(imfs, labels):
+    return list(set(imfs) - set(labels.keys()))
 
 
 with open('labels.json') as f:
    labels = json.load(f)
 
+imageFileNames = remove_done(os.listdir('photos'), labels)
+print(imageFileNames)
+counter = 0
+
+
+@bottle.get('/image')
+def getimage():
+    global counter
+    if counter == len(imageFileNames):
+        print("\nALL DONE!\n")
+        return
+    return bottle.static_file(imageFileNames[counter], root='photos')
+
 
 @bottle.post('/')
 def postmain():
-    labels.append(bottle.request.body.read())
+    global counter
+    if counter == len(imageFileNames):
+        print("\nALL DONE!\n")
+        return
+    choices = json.loads(str(bottle.request.body.read().decode()))
+    labels[imageFileNames[counter]] = choices
     with open('labels.json', 'w') as f:
         json.dump(labels, f)
     counter += 1
+
+
+bottle.run(host='localhost', port=3000, debug=True, server='paste')
---
+++ to_greyscale.py
@@ -0,0 +1,3 @@
+import os
+
+image_dirs
